05a120392eca671cd08f815dd7ee0a79bd0eb3b2
Thunderrock424242 <masonjoshua8@gmail.com>
2025-04-20 00:25:31 -0400

    fixed a lot of things and now it works and runs just needs testing on my modpack

---
+++ src/main/java/com/thunder/debugguardian/DebugGuardian.java
@@ -0,0 +1,79 @@
+package com.thunder.debugguardian;
+
+import com.thunder.debugguardian.debug.CrashHelper.CrashHandler;
+import com.thunder.debugguardian.debug.CrashHelper.LiveLogMonitor;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.network.handling.IPayloadHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Mod(DebugGuardian.MOD_ID)
+
+public class DebugGuardian {
+    /**
+     * The constant LOGGER.
+     */
+    public static final Logger LOGGER = LogManager.getLogger("debugguardian");
+
+
+    /**
+     * The constant MOD_ID.
+     */
+    public static final String MOD_ID = "debugguardian";
+    private static final Map<CustomPacketPayload.Type<?>, NetworkMessage<?>> MESSAGES = new HashMap<>();
+
+    private record NetworkMessage<T extends CustomPacketPayload>(StreamCodec<? extends FriendlyByteBuf, T> reader,
+                                                                 IPayloadHandler<T> handler) {
+    }
+
+    /**
+     * Instantiates a new Wilderness odyssey api main mod class.
+     *
+     * @param modEventBus the mod event bus
+     * @param container   the container
+     */
+    public DebugGuardian(IEventBus modEventBus, ModContainer container) {
+        LOGGER.info("WildernessOdysseyAPI initialized. I will also start to track mod conflicts");
+        // Register mod setup and creative tabs
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreative);
+
+        // Register global events
+        NeoForge.EVENT_BUS.register(this);
+
+        CrashHandler.register();
+        LiveLogMonitor.start();
+
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+}
+
+
+
+
---
+++ src/main/java/com/thunder/debugguardian/mixin/GlobalExceptionMixin.java
@@ -0,0 +1,20 @@
+package com.thunder.debugguardian.mixin;
+
+import com.thunder.debugguardian.debug.CrashHelper.LiveLogMonitor;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public abstract class GlobalExceptionMixin {
+
+    @Inject(method = "run", at = @At("HEAD"))
+    private void onGameStart(CallbackInfo ci) {
+        // Global uncaught exception handler
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            LiveLogMonitor.captureThrowable(throwable);
+        });
+    }
+}
